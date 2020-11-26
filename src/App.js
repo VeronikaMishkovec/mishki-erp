f155517a3a4d4d63d3c0b1f6b646f629125dbde7
VeronikaMishkovec <veronika.mishkovec@gmail.com>
2020-11-26 14:58:57 +0300

    add posts

--- src/App.js
+++ src/App.js
@@ -1,7 +1,26 @@
 import React from "react";
+import FetchedPosts from "./components/FetchedPosts";
+import PostForm from "./components/PostForm";
+import Posts from "./components/Posts";
 
 const App = () => {
-  return <h1>jbkbnl</h1>;
+  return (
+    <div className="container pt-3">
+      <div className="row">
+        <div className="col">
+          <PostForm />
+        </div>
+      </div>
+      <div className="row">
+        <div className="col">
+          <Posts />
+        </div>
+        <div className="col">
+          <FetchedPosts />
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default App;
---
+++ src/components/FetchedPosts.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const FetchedPosts = () => {
+  return (
+    <div>
+      <h1>FetchedPosts </h1>
+    </div>
+  );
+};
+
+export default FetchedPosts;
