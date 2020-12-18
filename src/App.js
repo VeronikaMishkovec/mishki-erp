c509cfdced16fe7bd9a1aea9f0119937b5eccba7
VeronikaMishkovec <veronika.mishkovec@gmail.com>
2020-12-18 10:44:41 +0300

    init

--- src/App.js
+++ src/App.js
@@ -1,28 +1,7 @@
 import React from "react";
-import FetchedPosts from "./components/FetchedPosts";
-import PostForm from "./components/PostForm";
-import Posts from "./components/Posts";
 
-const App = () => {
-  return (
-    <div className="container pt-3">
-      <div className="row">
-        <div className="col">
-          <PostForm />
-        </div>
-      </div>
-      <div className="row">
-        <div className="col">
-          <h2>Синхронные посты</h2>
-          <Posts posts={[]} />
-        </div>
-        <div className="col">
-          <h2>Асинхронные посты</h2>
-          <FetchedPosts posts={[]} />
-        </div>
-      </div>
-    </div>
-  );
-};
+function App() {
+  return <>Learn React</>;
+}
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,20 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { compose, createStore } from "redux";
-import { Provider } from "react-redux";
-import { rootReducer } from "./redux/rootReducer";
 import App from "./App";
 
-const store = createStore(
-  rootReducer,
-  compose(
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-  )
-);
-
 ReactDOM.render(
-  <Provider store={store}>
+  <React.StrictMode>
     <App />
-  </Provider>,
+  </React.StrictMode>,
   document.getElementById("root")
 );
