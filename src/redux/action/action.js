4632f4508f79d5b0299286e23e0a35bd64d87548
VeronikaMishkovec <veronika.mishkovec@gmail.com>
2020-12-23 10:49:20 +0300

    add redux

--- src/redux/action/action.js
+++ src/redux/action/action.js
@@ -1,11 +1,15 @@
-import firebase from 'firebase'
+import firebase from "firebase";
 
 export const getDate = () => {
-  const db = firebase.database()
-  const date = db.ref("date");
-  console.log('action', date)
+  const db = firebase.database();
+  const date = db.ref("date")
+  const dateDb = date.on("value", (el) => {
+    return el.val();
+  });
+  console.log("action", dateDb);
   return {
     type: "START_DATE",
-    payload: date,
+    payload: dateDb,
   };
 };
+
